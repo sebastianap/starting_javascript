b90d3b8f5e5a390c5da7c7a0dab002794ddac5c8
= <=>
2018-06-29 18:12:31 -0400

    listas

---
+++ listas.js
@@ -0,0 +1,44 @@
+// asi declaro una variable
+
+var nombre_d,nombre_j,nombre_di,nombre_m;
+
+// asi asigno valores a una variable
+
+nombre_d = "darlyn";
+nombre_j = "joel";
+nombre_di = "diego";
+nombre_m = "mauricio";
+
+// asi declaro una lista
+
+var nombres = [];
+console.log(nombres);
+// asi agrego elementos a mi lista
+
+nombres.push(nombre_d);
+console.log(nombres);
+nombres.push(nombre_j);
+console.log(nombres);
+nombres.push(nombre_di);
+console.log(nombres);
+nombres.push(nombre_m);
+console.log(nombres);
+//nombre_m = nombres.pop();
+//console.log(nombre_m);
+//console.log(nombres);
+
+// ejercicio1
+//obtener las variables con sub indices
+
+nombre_m = nombres.pop();
+console.log(nombre_m);
+console.log(nombres);
+nombre_di = nombres.pop();
+console.log(nombre_di);
+console.log(nombres);
+nombre_j = nombres.pop();
+console.log(nombre_j);
+console.log(nombres);
+nombre_d = nombres.pop();
+console.log(nombre_d);
+console.log(nombres);
